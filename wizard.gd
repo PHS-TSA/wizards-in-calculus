1eb2df8b4a3be1123410b79edb6cf13164882664
iiAzui <78660396+iiAzui@users.noreply.github.com>
2024-01-31 19:07:53 -0600

    Update wizard.gd
    Signed-off-by: iiAzui <78660396+iiAzui@users.noreply.github.com>

--- wizard.gd
+++ wizard.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
 
-@export var SPEED: int #you can guess
-@export var JUMP_VELOCITY = -400.0 #jump height kinda
-@export var rampin = 20 #how fast to go towards full speed
-@export var floaty: float #how far can jump
-@export var feather: int #how fast stop after space let go
-@export var friction: int #slow down kid
+@export var SPEED: int = 200 ## You can guess.
+@export var JUMP_VELOCITY = -350.0 ## Jump height, kinda.
+@export var rampin = 5 ## How fast to go towards full speed.
+@export var floaty: float = 150 ## How far the whizard can jump.
+@export var feather: int = 20## How fast the wizard should stop after they stop jumping.
+@export var friction: int = 25 ## Slow down, kid!
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,14 +19,16 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 	if Input.is_action_just_released("jump") and not is_on_floor() and not velocity.y > 0:
 		velocity.y = move_toward(velocity.y,0,(feather+(abs(velocity.y/2))))
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
 	var direction = Input.get_axis("left","right")
 	if direction:
-		velocity.x = move_toward(velocity.x,direction * SPEED,rampin+(abs(velocity.x))) #basicly takes rampin adds velocity over and over again
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, direction * SPEED, rampin) # This basically just speeds up by rampin.
+		else:
+			velocity.x = move_toward(velocity.x, direction * SPEED, rampin*2) # This basically just speeds up by rampin.
 		if direction == 1:
 			%WizardSprite.flip_h = true
 		elif direction == -1:
