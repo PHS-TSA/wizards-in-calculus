9f8d30335fac51ec3e1ebb787bfb73db0b1184aa
Eli <88557639+lishaduck@users.noreply.github.com>
2024-01-31 21:19:00 -0600

    style: remove whitespace, update comments

--- NumberOne.gd
+++ NumberOne.gd
@@ -2,10 +2,7 @@ extends Area2D
 
 var travelled_distance = 0
 
-
-# Called when the node enters the scene tree for the first time.
-
-
+## Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	const SPEED = 1000
 	const RANGE = 1200
@@ -17,5 +14,3 @@ func _physics_process(delta):
 
 	if (travelled_distance>RANGE):
 		queue_free()
-
-
