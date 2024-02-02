66566ca2ca777c3edd564954b8afb1a8fe0d5af2
iiAzui <78660396+iiAzui@users.noreply.github.com>
2024-02-02 13:37:38 -0600

    Add files via upload
    Signed-off-by: iiAzui <78660396+iiAzui@users.noreply.github.com>

---
+++ Spawnables/rock.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+@export var answer: int
+@export var question: String = "N/A"
+
+func _ready():
+	get_node("Sprite2D/Label").text = question
+	
+func take_damage(value: int) -> void:
+	if value == answer:
+		queue_free()
--- number_balls/old/number_two.gd
+++ number_balls/old/number_two.gd
@@ -1,19 +1,24 @@
 extends Area2D
 
 var travelled_distance = 0
-var num
+
+const num = 2
+
 
 func _physics_process(delta: float):
 	const SPEED = 300
 	const RANGE = 1200
+
 	var direction = Vector2.RIGHT.rotated(rotation)
-	get_node("Sprite2D").texture = load("res://number_balls/ball_sprites/"+str(num)+".png")
+	
 	if direction.x < 0:
 		$Sprite2D.flip_v = true
 		$Sprite2D.flip_h = true
+	
 	position += direction * SPEED * delta
+
 	travelled_distance += SPEED * delta
-	#get_node("Sprite2D/Label").text = str(num)
+
 	if travelled_distance > RANGE:
 		queue_free()
 
