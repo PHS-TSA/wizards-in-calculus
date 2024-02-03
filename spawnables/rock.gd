030ce05be51fdf33c62c38e7f18abd17ccf32379
iiAzui <78660396+iiAzui@users.noreply.github.com>
2024-02-03 07:16:49 -0600

    Juice Time
    added anti math juice
    
    Signed-off-by: iiAzui <78660396+iiAzui@users.noreply.github.com>

--- spawnables/rock.gd
+++ spawnables/rock.gd
@@ -2,14 +2,17 @@ extends StaticBody2D
 
 @export var answer: int
 @export var question: String = "N/A"
-
-@onready var label := %RockLabel
-
+@export var diffculty: int = 1
 
 func _ready():
-	label.question = question
+	get_node("Sprite2D/Label").text = question
 
 
 func take_damage(value: int) -> void:
 	if value == answer:
 		queue_free()
+		get_parent().get_node("CharacterBody2D").score += randi_range((1* (diffculty*3)) if diffculty > 0 else 1,(6 * diffculty) if diffculty > 0 else 2) 
+		get_parent().get_node("CharacterBody2D").get_node("Camera2D/Score").text = "Score: "+str(get_parent().get_node("CharacterBody2D").score)
+	else:
+		get_parent().get_node("CharacterBody2D").jucies -= 1
+		get_parent().get_node("CharacterBody2D").get_node("Camera2D/Juice").text = "Jucies: "+str(get_parent().get_node("CharacterBody2D").jucies)
