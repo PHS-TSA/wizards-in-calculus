20cd10099258c74be67e8b08d01ac29f98744dce
iiAzui <78660396+iiAzui@users.noreply.github.com>
2024-02-03 07:16:49 -0600

    feat: it's juice time!
    I've added the anti-math juice.
    
    Signed-off-by: iiAzui <78660396+iiAzui@users.noreply.github.com>

---
+++ spawnables/AMJ.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+func _on_body_exited():
+	get_parent().get_node("CharacterBody2D").jucies -= 5
+	get_parent().get_node("CharacterBody2D").get_node("Camera2D/Juice").text = "Jucies: "+str(get_parent().get_node("CharacterBody2D").jucies)
+	get_parent().get_node("CharacterBody2D").position = get_parent().get_node("Spawn").global_position
