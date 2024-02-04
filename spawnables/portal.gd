43e1dd965cc74623d747595e37efb0978af662bc
Eli <88557639+lishaduck@users.noreply.github.com>
2024-02-03 22:22:20 -0600

    fix: remove old line

--- spawnables/portal.gd
+++ spawnables/portal.gd
@@ -4,5 +4,4 @@ extends Area2D
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("entered!")  # Why is this called immediately?
 	get_tree().call_deferred("change_scene_to_file", "res://%s.tscn" % scene)
