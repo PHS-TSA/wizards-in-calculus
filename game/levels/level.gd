56acada3b9aeb105019bfbb659f5721fda578a99
Eli <88557639+lishaduck@users.noreply.github.com>
2024-02-06 17:14:14 -0600

    fix: move number balls to be by wizard hand

--- game/levels/level.gd
+++ game/levels/level.gd
@@ -4,10 +4,10 @@ const Ball := preload("res://game/spawnables/number/number.tscn")
 
 
 func _on_wizard_did_fire(ball: int, direction: float, location: Vector2) -> void:
-	print("FIRING!")
 	var spawned = Ball.instantiate()
 	add_child(spawned)
 	spawned.num = ball
 	spawned.rotation = direction
-	spawned.position = location
+	spawned.position = location + Vector2(12, -8)
 	spawned.velocity = spawned.velocity.rotated(direction)
+	spawned.z_index = 20
