ee5836f761ec14d65d31cbd0c251dfcc8da3b2f6
Eli <88557639+lishaduck@users.noreply.github.com>
2024-02-06 09:26:11 -0600

    feat: add heads up display

---
+++ game/components/heads_up.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@export var mana: int = 20:
+	set(value):
+		mana = value
+		mana_label.text = "Mana: %s" % mana
+
+@export var score: int = 0:
+	set(value):
+		score = value
+		score_label.text = "Score: %s" % score
+
+@onready var mana_label: Label = $ManaLabel
+@onready var score_label: Label = $ScoreLabel
+
+
+func _on_wizard_score_updated(value: int) -> void:
+	score = value
+
+
+func _on_wizard_mana_updated(value: int) -> void:
+	mana = value
