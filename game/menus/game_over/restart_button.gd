42c7112b309dd052a57c10b7a58e13befa056336
Eli <88557639+lishaduck@users.noreply.github.com>
2024-02-27 21:16:54 -0600

    fix: errors about queue_free when in physics

--- game/menus/game_over/restart_button.gd
+++ game/menus/game_over/restart_button.gd
@@ -3,4 +3,4 @@ extends Button
 
 
 func _pressed() -> void:
-	var _tree := self.get_tree().change_scene_to_file("res://game/levels/tutorial.tscn")
+	self.get_tree().change_scene_to_file.call_deferred("res://game/levels/tutorial.tscn")
