4fcc8329b189569fed945a356dab3d9555e46c4c
Eli <88557639+lishaduck@users.noreply.github.com>
2024-02-27 22:40:48 -0600

    chore: remove duplicate script

--- game/menus/game_over/restart_button.gd
+++
@@ -1,6 +0,0 @@
-class_name RestartButton
-extends Button
-
-
-func _pressed() -> void:
-	self.get_tree().change_scene_to_file.call_deferred("res://game/levels/tutorial.tscn")
--- game/menus/tutorial_button.gd
+++ game/menus/tutorial_button.gd
@@ -1,4 +1,4 @@
-class_name StartButton
+class_name TutorialButton
 extends Button
 
 
